ca55a6fb3364e1cd475aee008b978f4a3e54a442
Yw2 Theorycrafter <yw2theorycrafter@gmail.com>
2024-12-19 01:41:04 -0800

    make hab building work

---
+++ Patches/BuilderPatch.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using HarmonyLib;
+using UnityEngine;
+
+namespace com.yw2theorycrafter.thirdpersonview {
+    [HarmonyPatch(typeof(Builder), nameof(Builder.GetAimTransform))]
+    class Builder_GetAimTransformPatch{
+        [HarmonyPrefix]
+        public static bool Prefix(ref Transform __result) {
+            var thirdPersonControl = ThirdPersonCameraControl.main;
+            bool ret = true;
+            if (thirdPersonControl && thirdPersonControl.enabled)
+            {
+                __result = thirdPersonControl.transform;
+                ret = false;
+            }
+            return ret;
+        }
+    }
+}
